c06a2197275cd8d187395e3db14ac358e6af46cd
antonalalaev <antonalalaev@gmail.com>
2016-06-27 17:26:16 +0500

    Made snake

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -20,6 +20,10 @@ namespace Snake
             VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
             leftLine.Draw();
             rightLine.Draw();
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
+
             Console.ReadKey();
         }
 
