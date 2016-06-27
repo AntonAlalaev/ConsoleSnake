77c2c80b9c60c417ec6be629ffbdf6dc1fa2b278
antonalalaev <antonalalaev@gmail.com>
2016-06-27 20:41:34 +0500

    ready Snake console game

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -13,17 +13,19 @@ namespace Snake
         {
             //Console.SetBufferSize( 80, 24);
             Console.SetWindowSize(80,25);
-            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
-            Draw(upLine);
-            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
-            Draw(downLine);
-            
-            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
-            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
-            Draw(leftLine);
-            Draw(rightLine);
-            
-            Point p = new Point(10, 5, '*');
+            //HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            //Draw(upLine);
+            //HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            //Draw(downLine);
+
+            //VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            //VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            //Draw(leftLine);
+            //Draw(rightLine);
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
+
+            Point p = new Point(4, 5, '*');
             Figure fSnake = new Snake(p, 4, Direction.RIGHT);
             Draw(fSnake);
             Snake snake = (Snake)fSnake;
@@ -33,6 +35,10 @@ namespace Snake
             food.Draw();
             while (true)
             {
+                if (walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
                 if (snake.Eat(food))
                 {
                     food = foodCreator.CreateFood();
@@ -42,7 +48,6 @@ namespace Snake
                 {
                     snake.Move();
                 }
-
                 Thread.Sleep(100);
 
                 if (Console.KeyAvailable)
