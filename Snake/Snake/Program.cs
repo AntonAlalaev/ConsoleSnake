6f552078f9a898e50ed945c77b997d508a9ab309
antonalalaev <antonalalaev@gmail.com>
2016-06-27 18:14:47 +0500

    the snake moves can be controlled by the keyboard keys

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -24,23 +24,18 @@ namespace Snake
             Point p = new Point(10, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
 
-            Console.ReadKey();
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
+                Thread.Sleep(100);
+                snake.Move();
+            }
+            //Console.ReadKey();
         }
 
     }
